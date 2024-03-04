f912b597c8de0dabb85e7b815394ff2192429a9a
Duggu-Jhansi <duggujhansireddy123@gmail.com>
2024-03-04 17:01:12 +0530

    commit Message

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules:["@nuxtjs/tailwindcss"],
+  runtimeConfig:{
+    movieKey:process.env.MOVIE_API_KEY,
+  },
+  app:{ pageTransition:{name:'page',mode:'out-in'},},
+})
